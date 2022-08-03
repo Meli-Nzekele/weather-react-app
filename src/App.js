9a827b4d25b5ae6eb2ca00c550b18634f4dadde9
Meli-Nzekele <melissanzekelecourtney@gmail.com>
2022-08-03 10:55:03 +0100

    Added Bootstrap, Google fonts & Font Awesome  to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <button className="btn btn-primary">Hello</button>
+        <header className="App-header">Weather App</header>
+      </div>
     </div>
   );
 }
