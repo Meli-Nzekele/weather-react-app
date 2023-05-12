7b25ba04bf70363b1b8fbad350cba4edede55851
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-05-12 14:46:58 +0100

    move footer code in App component into Footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Weather from "./Weather";
+import Footer from "./Footer";
 
 import "./App.css";
 
@@ -7,34 +8,8 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="London" />
-        <footer className="Footer">
-          This App was built by
-          <a
-            href="https://incandescent-tapioca-00054a.netlify.app/"
-            rel="noreferrer"
-            target="_blank"
-          >
-            {" "}
-            Melissa Nzekele,
-          </a>{" "}
-          open-sourced on{" "}
-          <a
-            href="https://github.com/Meli-Nzekele/weather-react-app"
-            rel="noreferrer"
-            target="_blank"
-          >
-            GitHub
-          </a>{" "}
-          & Hosted on{" "}
-          <a
-            href="https://app.netlify.com/sites/wonderful-bavarois-367c41/overview"
-            rel="noreferrer"
-            target="_blank"
-          >
-            Netlify
-          </a>
-        </footer>
+        <Weather defaultCity="Manchester" />
+        <Footer />
       </div>
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,31 @@
+export default function Footer() {
+  return (
+    <footer className="Footer">
+      This App was built by
+      <a
+        href="https://incandescent-tapioca-00054a.netlify.app/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        {" "}
+        Melissa Nzekele,
+      </a>{" "}
+      open-sourced on{" "}
+      <a
+        href="https://github.com/Meli-Nzekele/weather-react-app"
+        rel="noreferrer"
+        target="_blank"
+      >
+        GitHub
+      </a>{" "}
+      & Hosted on{" "}
+      <a
+        href="https://app.netlify.com/sites/wonderful-bavarois-367c41/overview"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Netlify
+      </a>
+    </footer>
+  );
+}
